d2ebe528782ed6d92305d83e4551ca3a3c900ffb
Leon-ahmed <mdleon1200@gmail.com>
2026-04-28 18:38:41 +0600

    Responsive Navbar for mobile device done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,15 +5,15 @@ import { ImStatsDots } from "react-icons/im";
 const Navbar = () => {
     return (
         <div>
-            <div class="px-10 py-4 flex navbar bg-base-100 shadow-sm">
-                        <div class="flex-1">
-                              <a class="btn btn-ghost text-2xl font-semibold text-green-primary"><span className='text-2xl font-extrabold text-black'>Keen</span>Keeper</a>
+            <div class="px-10 py-4 flex flex-col md:flex-row navbar space-y-3 bg-base-100 shadow-sm">
+                        <div class="flex-1 w-6/12 mx-auto  ">
+                              <a class="btn btn-ghost text-2xl   font-semibold text-green-primary"><span className='text-2xl font-extrabold text-black'>Keen</span>Keeper</a>
                         </div>
           <div>
-                  <ul class="flex gap-2   menu menu-horizontal px-1">
+                  <ul class="flex flex-col w-6/12 mx-auto   md:flex-row md:w-full   gap-2   menu menu-horizontal px-1">
                       <li className='flex items-center gap-1 text-gray bg-green-primary px-4 py-3 rounded-md'><RiHome2Line /><a>Home</a></li>
-                       <li className='flex items-center gap-1 text-gray'><FaRegClock /><a>Timeline</a></li>
-                        <li className='flex items-center gap-1 text-gray'><ImStatsDots /><a>Stats</a></li>
+                       <li className='flex items-center gap-1 text-gray px-4 py-3'><FaRegClock /><a>Timeline</a></li>
+                        <li className='flex items-center gap-1 text-gray px-4 py-3'><ImStatsDots /><a>Stats</a></li>
       
                      </ul>
              </div>
